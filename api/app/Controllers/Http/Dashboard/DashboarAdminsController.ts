66da62c2ff0c93f6cfd1d4e6a88d1bfdac52386d
imtiaz <imtiazahmed026@gmail.com>
2022-04-21 14:17:02 +0600

    working on admin dashboard(updating status of requested registrations)

--- api/app/Controllers/Http/Dashboard/DashboarAdminsController.ts
+++ api/app/Controllers/Http/Dashboard/DashboarAdminsController.ts
@@ -1,8 +1,27 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class DashboarAdminsController {
     public async test(ctx: HttpContextContract) {
         return { msg: "dashboard running" }
 
+    }
+    public async requested(ctx: HttpContextContract) {
+        const users = await User.query().where("status", "")
+        // console.log(users[0]);
+        return users
+    }
+    public async updateStatus(ctx: HttpContextContract) {
+        // update users registration request
+        /**
+            * 0 -> pending
+            * 1 -> accepted
+            * 2 -> rejected
+            */
+        const data = ctx.request.all();
+        console.log(data)
+        const user = await User.query().where("id", "id")
+            .update(data);
+        return user
     }
 }
--- api/app/Controllers/Http/Dashboard/dashboard.ts
+++ api/app/Controllers/Http/Dashboard/dashboard.ts
@@ -2,4 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
     Route.get("/", "Dashboard/DashboarAdminsController.test")
+    Route.get("/request", "Dashboard/DashboarAdminsController.requested")
+    Route.post("/update/status", "Dashboard/DashboarAdminsController.updateStatus")
 }).prefix("dashboard")
