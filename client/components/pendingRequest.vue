18c678dc8e776eae95f6894e12563f5f39869930
imtiaz <imtiazahmed026@gmail.com>
2022-04-21 16:05:45 +0600

    working on admins dashboard

---
+++ client/components/pendingRequest.vue
@@ -0,0 +1,20 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			requests: null,
+		};
+	},
+	async created() {
+		const res = await this.callApi("get", "dashboard/request");
+		console.log(res);
+	},
+};
+</script>
+
+<style>
+</style>
