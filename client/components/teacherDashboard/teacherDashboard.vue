32507c0a09e55a25d836347b520dd2d4dfa5e9b6
imtiaz <imtiazahmed026@gmail.com>
2022-04-24 15:28:10 +0600

    working on setting up time slots for a teacher

--- client/components/teacherDashboard/teacherDashboard.vue
+++ client/components/teacherDashboard/teacherDashboard.vue
@@ -12,16 +12,47 @@
 				style="marging: 30px 20px"
 				class="_log_btn _2menu_long"
 				type="button"
-				v-on:click="seeStudentList()"
 			>
 				History
 			</button>
+			<button
+				style="marging: 30px 20px"
+				class="_log_btn _2menu_long"
+				type="button"
+			>
+				Add time slots
+			</button>
+		</div>
+		<div>
+			<!-- <client-only>
+				<date-picker v-model="date_today" format="HH:MM" />
+			</client-only> -->
+			<vue-timepicker v-model="date_today"></vue-timepicker>
+		</div>
+		<div>
+			<button v-on:click="time">check</button>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import VueTimepicker from "vue2-timepicker/src/vue-timepicker.vue";
+
+export default {
+	components: {
+		"vue-timepicker": VueTimepicker,
+	},
+	data() {
+		return {
+			date_today: "",
+		};
+	},
+	methods: {
+		time() {
+			console.log(this.date_today);
+		},
+	},
+};
 </script>
 
 <style>
