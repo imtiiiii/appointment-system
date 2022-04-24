b35d6dab01ea9bc590060c4288d5017fe1ab649d
imtiaz <imtiazahmed026@gmail.com>
2022-04-24 16:59:32 +0600

    adding slots to db from a teacher is complete without checking for any error / validation

--- api/app/Controllers/Http/TimeSlots/TimeSlotsController.ts
+++ api/app/Controllers/Http/TimeSlots/TimeSlotsController.ts
@@ -1,8 +1,10 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import TimeSlot from '../../../Models/TimeSlot'
 export default class TimeSlotsController {
     public async add(ctx: HttpContextContract) {
         const data = ctx.request.all();
         console.log("data=", data)
+        const time = await TimeSlot.create(data);
+        return time;
     }
 }
