fb287857916d0ea54866d1d9bafc2839550be924
imtiaz <imtiazahmed026@gmail.com>
2022-04-26 17:05:11 +0600

    working on student's dashboard

--- client/components/adminDashboard/teacherList.vue
+++ client/components/adminDashboard/teacherList.vue
@@ -7,36 +7,50 @@
 		>
 			<div style="background-color: #9cb0ba; padding: 10px 20px">
 				<h4>
-					name: {{ teacher.first_name + " " + teacher.last_name }}
+					Name: {{ teacher.first_name + " " + teacher.last_name }}
 				</h4>
 				<hr />
-				<h4>email:{{ teacher.email }}</h4>
+				<h4>Email:{{ teacher.email }}</h4>
 				<hr />
-				<h4>identity:{{ teacher.user_type }}</h4>
+				<h4>Identity:{{ teacher.user_type }}</h4>
 				<hr />
-
-				<div style="margin: 30px 0px">
+				<div v-if="user.user_type === 'admin'">
+					<div style="margin: 30px 0px">
+						<button
+							v-on:click.once="update(teacher.id)"
+							class="update"
+							style="
+								background-color: #42cc8c;
+								width: 100px;
+								padding: 15px 0px;
+							"
+						>
+							Update
+						</button>
+						<button
+							v-on:click.once="erase(teacher.id)"
+							class="reject"
+							style="
+								background-color: #ff531d;
+								width: 100px;
+								padding: 15px 0px;
+							"
+						>
+							Delete
+						</button>
+					</div>
+				</div>
+				<div v-else>
 					<button
-						v-on:click.once="update(teacher.id)"
 						class="update"
 						style="
 							background-color: #42cc8c;
-							width: 100px;
+							width: 100%;
 							padding: 15px 0px;
 						"
 					>
-						Update
-					</button>
-					<button
-						v-on:click.once="erase(teacher.id)"
-						class="reject"
-						style="
-							background-color: #ff531d;
-							width: 100px;
-							padding: 15px 0px;
-						"
-					>
-						Delete
+						See {{ teacher.first_name + " " + teacher.last_name }}'s
+						time slots
 					</button>
 				</div>
 			</div>
@@ -49,9 +63,11 @@ export default {
 	data() {
 		return {
 			teachers: null,
+			user: null,
 		};
 	},
 	async created() {
+		this.user = this.$store.state.authUser;
 		const teachers = await this.callApi("get", "/dashboard/teacher-list/");
 		if (teachers.status === 200) {
 			this.teachers = teachers.data;
--- client/components/studentDashboard/studentDashboard.vue
+++ client/components/studentDashboard/studentDashboard.vue
@@ -1,7 +1,50 @@
-<template></template>
+<template>
+	<div>
+		<div>
+			<h3>Welcome to student's dashboard</h3>
+		</div>
+		<!-- ALL BUTTONS -->
+		<div>
+			<button
+				style="marging: 30px 20px"
+				class="_log_btn _2menu_long"
+				type="button"
+			>
+				History
+			</button>
+			<button
+				style="marging: 30px 20px"
+				class="_log_btn _2menu_long"
+				type="button"
+			>
+				Upcoming appointments
+			</button>
+			<button
+				style="marging: 30px 20px"
+				class="_log_btn _2menu_long"
+				type="button"
+			>
+				Teacher list
+			</button>
+			<!-- <button
+				style="marging: 30px 20px"
+				class="_log_btn _2menu_long"
+				type="button"
+			>
+				Search
+			</button> -->
+		</div>
+		<div>
+			<teacher-list></teacher-list>
+		</div>
+	</div>
+</template>
 
 <script>
-export default {};
+import teacherList from "../adminDashboard/teacherList.vue";
+export default {
+	components: { teacherList },
+};
 </script>
 
 <style>
