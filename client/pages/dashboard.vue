f6f3cb755c4728c07e969950d7659d3f3d5e890c
imtiaz <imtiazahmed026@gmail.com>
2022-04-26 15:37:34 +0600

    starting to work on student's dashboard

--- client/pages/dashboard.vue
+++ client/pages/dashboard.vue
@@ -8,16 +8,21 @@
 			<h3>Teacher dashboard</h3>
 			<teacher-dashboard></teacher-dashboard>
 		</div>
+		<div v-if="user.user_type === 'student'">
+			<h3>Student dashboard</h3>
+			<student-dashboard></student-dashboard>
+		</div>
 	</div>
 </template>
 
 <script>
 import adminDashboard from "../components/adminDashboard/adminDashboard.vue";
+import StudentDashboard from "../components/studentDashboard/studentDashboard.vue";
 import TeacherDashboard from "../components/teacherDashboard/teacherDashboard.vue";
 
 export default {
 	middleware: "auth",
-	components: { adminDashboard, TeacherDashboard },
+	components: { adminDashboard, TeacherDashboard, StudentDashboard },
 	data() {
 		return {
 			user: null,
