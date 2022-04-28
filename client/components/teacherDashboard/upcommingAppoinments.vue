6f2b6a4fc795f5cc80786e2e8ce80ec5691d403c
slow__rider <akashcsemu@gmail.com>
2022-04-28 16:57:11 +0600

    fix: Profile for own and other is fixed

--- client/components/teacherDashboard/upcommingAppoinments.vue
+++ client/components/teacherDashboard/upcommingAppoinments.vue
@@ -16,16 +16,16 @@
                     <tr v-for="(item,index) in upCommingAppoinments" :key="index">
                         <th>{{item.date}}</th>
                         <th>{{item.forWhichTimeSlot.day.day_name}}</th>
-                        <th>{{item.byWhichStudent.user_name}}</th>
+                        <th><button id="link" v-on:click="jumpToProfile(item.byWhichStudent.id)">{{item.byWhichStudent.user_name}}</button> </th>
                         <th>{{item.byWhichStudent.email}}</th>
                         <th>{{item.byWhichStudent.dept}}</th>
                         <th>{{item.byWhichStudent.course}}</th>
                         <th>{{item.agenda}}</th>
                         <th>
                             <!-- acceptAppointment(item.id,index,1) here 1 means enum value of status accept -->
-                            <button v-on:click="changeStatus(item.id,index,'1')">Accept</button>
+                            <Button v-on:click="changeStatus(item.id,index,'1')">Accept</Button>
                             <!-- acceptAppointment(item.id,index,1) here 2 means enum value of status rejection -->
-                            <button v-on:click="changeStatus(item.id,index,'2')">Reject</button>
+                            <Button v-on:click="changeStatus(item.id,index,'2')">Reject</Button>
                         </th>
                     </tr>
                 </table>
@@ -62,6 +62,9 @@ export default {
                 this.swr();
             }
             
+        },
+        jumpToProfile(profileId){
+            this.$router.push(`profile/${profileId}`)
         }
     }
 }
@@ -69,18 +72,29 @@ export default {
 
 <style>
     table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
     }
 
     td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
     }
 
     tr:nth-child(even) {
-    background-color: #dddddd;
+        background-color: #dddddd;
+    }
+    button#link {
+        background: none!important;
+        border: none;
+        padding: 0!important;
+        /*optional*/
+        font-family: arial, sans-serif;
+        /*input has OS specific font-family*/
+        color: #069;
+        text-decoration: underline;
+        cursor: pointer;
     }
 </style>
--- client/pages/profile/_id.vue
+++ client/pages/profile/_id.vue
@@ -3,22 +3,6 @@
 		<profile-details
 			v-bind:userId="this.$route.params.id"
 		></profile-details>
-		<div>
-			<button
-				v-on:click="editProfile = !editProfile"
-				style="
-					background-color: #42cc8c;
-					width: 100px;
-					padding: 15px 0px;
-					margin: 10px 20px;
-				"
-			>
-				{{ editProfile ? "Undo" : "Edit Profile" }}
-			</button>
-		</div>
-		<div v-if="editProfile" style="margin-left: 20px">
-			<edit-profile v-bind:userId="userId"></edit-profile>
-		</div>
 	</div>
 </template>
 
@@ -36,23 +20,7 @@ export default {
 	},
 	async created() {
 		this.userId = this.$route.params.id;
-		console.log("user type", this.$store.state.authUser.user_type);
-		console.log("user id", this.$store.state.authUser.id);
-		console.log("router id", this.userId);
-		if (
-			this.$store.state.authUser.user_type !== "admin" &&
-			this.$store.state.authUser.id !== this.userId
-		) {
-			this.$router.push("/profile");
-		}
 	},
-	mounted() {
-		// if (
-		// 	this.$store.state.authUser.user_type !== "admin" &&
-		// 	this.userId !== this.$store.state.authUser.id
-		// ) {
-		// 	this.$router.push("/profile");
-		// }
-	},
+
 };
 </script>
