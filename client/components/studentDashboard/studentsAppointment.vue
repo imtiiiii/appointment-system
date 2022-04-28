81e33ce6794e615cce5e0eeae9dfa33b67dfba02
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-04-29 01:51:03 +0600

    tried to improve front-end presentation

--- client/components/studentDashboard/studentsAppointment.vue
+++ client/components/studentDashboard/studentsAppointment.vue
@@ -1,112 +1,116 @@
 <template>
-	<div>
-		<div>welcome to your appointment {{ type }}</div>
-		<div
-			style="
-				height: 200vh;
-				background-color: white;
-				width: 70%;
-				margin: 30px auto;
-			"
-		>
-			<div class="slot">
-				<h6>Date</h6>
-				<h6>Agenda</h6>
-				<h6>time</h6>
-				<h6>teacher</h6>
-				<h6>dept</h6>
-			</div>
-			<div
-				class="slot"
-				v-for="(appointment, index) of appointments"
-				:key="index"
-			>
-				<h6>{{ appointment.date }}</h6>
-				<h6>{{ appointment.agenda }}</h6>
-				<h6>
-					{{ appointment.forWhichTimeSlot.start_time }}-{{
-						appointment.forWhichTimeSlot.end_time
-					}}
-				</h6>
-				<h6>
-					{{
-						appointment.forWhichTimeSlot.user.first_name +
-						" " +
-						appointment.forWhichTimeSlot.user.last_name
-					}}
-				</h6>
-				<h6>{{ appointment.forWhichTimeSlot.user.dept }}</h6>
-			</div>
-		</div>
-	</div>
+  <div>
+    <div>welcome to your appointment {{ type }}</div>
+    <div
+      v-if="appointments.length !== 0"
+      style="
+        height: 200vh;
+        background-color: white;
+        width: 70%;
+        margin: 30px auto;
+      "
+    >
+      <div class="slot">
+        <h6>Date</h6>
+        <h6>Agenda</h6>
+        <h6>time</h6>
+        <h6>teacher</h6>
+        <h6>dept</h6>
+      </div>
+      <div
+        class="slot"
+        v-for="(appointment, index) of appointments"
+        :key="index"
+      >
+        <h6>{{ appointment.date }}</h6>
+        <h6>{{ appointment.agenda }}</h6>
+        <h6>
+          {{ appointment.forWhichTimeSlot.start_time }}-{{
+            appointment.forWhichTimeSlot.end_time
+          }}
+        </h6>
+        <h6>
+          {{
+            appointment.forWhichTimeSlot.user.first_name +
+            " " +
+            appointment.forWhichTimeSlot.user.last_name
+          }}
+        </h6>
+        <h6>{{ appointment.forWhichTimeSlot.user.dept }}</h6>
+      </div>
+    </div>
+    <div v-else>
+      <h3>Nothing to show</h3>
+    </div>
+  </div>
 </template>
 
 <script>
 import moment from "moment";
 moment().format();
 export default {
-	props: ["type"],
-	data() {
-		return {
-			currentDate: null,
-			appointments: [],
-		};
-	},
-	watch: {
-		chk() {},
-		history() {},
-	},
-	computed: {
-		chk() {
-			let now = moment().toString();
-			this.currentDate = now;
-			// console.log(this.currentDate);
-		},
-		async history() {
-			this.appointments = [];
-			console.log("history called", this.currentDate);
+  props: ["type"],
+  data() {
+    return {
+      currentDate: null,
+      appointments: [],
+    };
+  },
+  watch: {
+    chk() {},
+    history() {},
+  },
+  computed: {
+    chk() {
+      let now = moment().toString();
+      this.currentDate = now;
+      // console.log(this.currentDate);
+    },
+    async history() {
+      this.appointments = [];
+      console.log("history called", this.currentDate);
 
-			const res = await this.callApi(
-				"get",
-				`/appointments?id=${this.$store.state.authUser.id}&type=${this.type}`
-			);
-			// this.appointments = res.data;
-			console.log(res.data);
-			for (let i of res.data) {
-				console.log("type= ", this.type);
-				if (this.type == "upcoming") {
-					let slot = moment(i.date);
-					let currDate = moment();
-					const compare = slot.isBefore(currDate);
-					// console.log("??=", slot.isBefore(currDate));
-					if (compare === false) {
-						i.date = moment(i.date).format("DD-MM-YYYY");
-						this.appointments.push(i);
-					}
-				} else if (this.type == "history") {
-					let slot = moment(i.date);
-					let currDate = moment();
-					const compare = slot.isBefore(currDate);
-					// console.log("??=", slot.isBefore(currDate));
-					if (compare === true) {
-						i.date = moment(i.date).format("DD-MM-YYYY");
-						this.appointments.push(i);
-					}
-				}
-			}
-			console.log("final", this.appointments);
-		},
-	},
+      const res = await this.callApi(
+        "get",
+        `/appointments?id=${this.$store.state.authUser.id}&type=${this.type}`
+      );
+      // this.appointments = res.data;
+      console.log(res.data);
+      for (let i of res.data) {
+        console.log("type= ", this.type);
+        if (this.type == "upcoming") {
+          let slot = moment(i.date);
+          let currDate = moment();
+          const compare = slot.isBefore(currDate);
+          // console.log("??=", slot.isBefore(currDate));
+          if (compare === false) {
+            i.date = moment(i.date).format("DD-MM-YYYY");
+            this.appointments.push(i);
+          }
+        } else if (this.type == "history") {
+          let slot = moment(i.date);
+          let currDate = moment();
+          const compare = slot.isBefore(currDate);
+          // console.log("??=", slot.isBefore(currDate));
+          if (compare === true) {
+            i.date = moment(i.date).format("DD-MM-YYYY");
+            this.appointments.push(i);
+          }
+        }
+      }
+      console.log("final", this.appointments);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .slot {
-	text-align: center;
-	display: grid;
-	grid-template-columns: repeat(5, 1fr);
-	/* grid-row-gap: 100px; */
-	grid-column-gap: 60px;
-	margin: 50px 10px;
+  text-align: center;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  /* grid-row-gap: 100px; */
+  grid-column-gap: 60px;
+  margin: 50px 10px;
 }
 </style>
--- client/pages/profile/_id.vue
+++ client/pages/profile/_id.vue
@@ -1,26 +1,47 @@
 <template>
-	<div style="background-color: #9cb0ba; margin: 10px 20px">
-		<profile-details
-			v-bind:userId="this.$route.params.id"
-		></profile-details>
-	</div>
+  <div>
+    <div>
+      <div style="background-color: #9cb0ba; margin: 10px 20px">
+        <profile-details
+          v-bind:userId="this.$route.params.id"
+        ></profile-details>
+      </div>
+    </div>
+    <div v-if="this.$store.state.authUser.user_type === 'admin'">
+      <div>
+        <button
+          v-on:click="editProfile = !editProfile"
+          style="
+            background-color: #42cc8c;
+            width: 100px;
+            padding: 15px 0px;
+            margin: 10px 20px;
+          "
+        >
+          {{ editProfile ? "Undo" : "Edit Profile" }}
+        </button>
+      </div>
+      <div v-if="editProfile" style="margin-left: 20px">
+        <edit-profile v-bind:userId="userId"></edit-profile>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import profileDetails from "../../components/profileDetails.vue";
 export default {
-	components: {
-		"profile-details": profileDetails,
-	},
-	data() {
-		return {
-			userId: null,
-			editProfile: false,
-		};
-	},
-	async created() {
-		this.userId = this.$route.params.id;
-	},
-
+  components: {
+    "profile-details": profileDetails,
+  },
+  data() {
+    return {
+      userId: null,
+      editProfile: false,
+    };
+  },
+  async created() {
+    this.userId = this.$route.params.id;
+  },
 };
 </script>
