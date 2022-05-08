b2811f2a306f0319a01f82f3b2199e3ef7c6a91a
akashkarmokar <akashcsemu@gmail.com>
2022-05-08 12:51:19 +0600

    fix: API - Appointment Request

--- api/app/Controllers/Http/Appointment/AppoinmentsQuery.ts
+++ api/app/Controllers/Http/Appointment/AppoinmentsQuery.ts
@@ -1,21 +1,29 @@
 import Appointment from "App/Models/Appointment";
 import moment from "moment";
-
+moment().format()
 export default class AppoinmentQuery{
     public async upCommingAppoinments(upCommingAppoinmentsFor){
         /**
          * Find all appoinments Request which is not accepted yet
          */
-        const cuurentDate = moment().toString();
-        let appoinments = await Appointment.query().select('*').preload('forWhichTimeSlot',(timeSlotQuery)=>{
-            timeSlotQuery.where('teacherId',upCommingAppoinmentsFor.teacherId).preload('day');
-        }).preload('byWhichStudent').where('date','>=',cuurentDate).andWhere('status','0').orderBy('date','desc');
+        let cuurentDate = moment().toString();
         // let appoinments = await Appointment.query().select('*').preload('forWhichTimeSlot',(timeSlotQuery)=>{
         //     timeSlotQuery.where('teacherId',upCommingAppoinmentsFor.teacherId).preload('day');
-        // }).preload('byWhichStudent')
+        // }).preload('byWhichStudent').where('date','>=',cuurentDate).andWhere('status','0').orderBy('date','desc');
+        let appoinments = await Appointment.query().select('*').preload('forWhichTimeSlot',(timeSlotQuery)=>{
+            timeSlotQuery.where('teacherId',upCommingAppoinmentsFor.teacherId).preload('day');
+        }).preload('byWhichStudent').andWhere('status','0').orderBy('date','desc');
         // let appoinments = await Appointment.query().select('*').preload('byWhichStudent');
-        const appoinmentsJSON = appoinments.map((appoinments)=> appoinments.serialize()) 
-        return appoinmentsJSON;
+        const appoinmentsJSON = appoinments.map((appoinments)=> appoinments.serialize())
+        const finalResult : object[] = [];
+        for(let x of appoinmentsJSON){
+            let dbDate = moment(x.date);
+            if(dbDate.isAfter(cuurentDate)){
+                finalResult.push(x);
+            }
+        } 
+        // return appoinmentsJSON;
+        return finalResult;
     }
 
     public async status(changeStatusFor){
