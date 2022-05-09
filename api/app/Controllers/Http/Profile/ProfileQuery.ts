857b247cffea88bf3907d60f67a1a62d8e08a51f
imtiaz <imtiazahmed026@gmail.com>
2022-05-09 11:06:56 +0600

    updating profile bug fixed

--- api/app/Controllers/Http/Profile/ProfileQuery.ts
+++ api/app/Controllers/Http/Profile/ProfileQuery.ts
@@ -1,14 +1,18 @@
 import User from "App/Models/User"
 
 export default class ProfileQuery {
-    async profileUpdate(data){
+    async profileUpdate(data) {
+        console.log("update data called", data);
         const user = await User.findOrFail(data.id);
-        Object.keys(data).forEach((value)=>{
-            if(value != 'id'){
-                user[value] = data[value];
-            }
-        })
-        // console.log(user);
-        await user.save()
+        console.log("user=", user)
+        console.log("loop starting")
+        // Object.keys(data).forEach((value) => {
+        //     console.log("value=", value)
+        //     if (value != 'id') {
+        //         user[value] = data[value];
+        //     }
+        // })
+        console.log("user at last= ", user);
+        await user.merge(data).save();
     }
 }
